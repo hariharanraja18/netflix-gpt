972c2c129964220a1ffc3b7a974ab1011cb1495b
hariharanraja18 <143488671+hariharanraja18@users.noreply.github.com>
2025-04-19 09:59:13 +0530

    Initial commit: Vite + Tailwind CSS config

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-red-600 text-3xl font-bold'>Namaste react lets build netflix gpt from scratch</h1>
+    </>
+  )
+}
+
+export default App
